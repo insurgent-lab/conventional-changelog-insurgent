eceb88a26794fb7d96233f5978feccadc49dd365
Pierre CAVIN <tech@sherlox.io>
2020-05-01 16:41:58 +0200

    test: fix tests

--- test/test.js
+++ test/test.js
@@ -83,7 +83,7 @@ describe('angular preset', function () {
         expect(chunk).to.include('amazing new module')
         expect(chunk).to.include('**compile:** avoid a bug')
         expect(chunk).to.include('make it faster')
-        expect(chunk).to.include(', closes [#1](https://github.com/conventional-changelog/conventional-changelog/issues/1) [#2](https://github.com/conventional-changelog/conventional-changelog/issues/2)')
+        expect(chunk).to.include(', closes [#1](https://github.com/insurgent-lab/conventional-changelog-insurgent/issues/1) [#2](https://github.com/insurgent-lab/conventional-changelog-insurgent/issues/2)')
         expect(chunk).to.include('New build system.')
         expect(chunk).to.include('Not backward compatible.')
         expect(chunk).to.include('**compile:** The Change is huge.')
@@ -119,7 +119,7 @@ describe('angular preset', function () {
       })
       .pipe(through(function (chunk) {
         chunk = chunk.toString()
-        expect(chunk).to.include('[#133](https://github.com/conventional-changelog/conventional-changelog/issues/133)')
+        expect(chunk).to.include('[#133](https://github.com/insurgent-lab/conventional-changelog-insurgent/issues/133)')
         done()
       }))
   })
@@ -135,8 +135,8 @@ describe('angular preset', function () {
       })
       .pipe(through(function (chunk) {
         chunk = chunk.toString()
-        expect(chunk).to.include('[#88](https://github.com/conventional-changelog/conventional-changelog/issues/88)')
-        expect(chunk).to.not.include('closes [#88](https://github.com/conventional-changelog/conventional-changelog/issues/88)')
+        expect(chunk).to.include('[#88](https://github.com/insurgent-lab/conventional-changelog-insurgent/issues/88)')
+        expect(chunk).to.not.include('closes [#88](https://github.com/insurgent-lab/conventional-changelog-insurgent/issues/88)')
         done()
       }))
   })
@@ -273,9 +273,9 @@ describe('angular preset', function () {
       .pipe(through(function (chunk, enc, cb) {
         chunk = chunk.toString()
 
-        expect(chunk).to.include('(https://github.com/conventional-changelog/conventional-changelog/compare')
-        expect(chunk).to.include('](https://github.com/conventional-changelog/conventional-changelog/commit/')
-        expect(chunk).to.include('](https://github.com/conventional-changelog/conventional-changelog/issues/')
+        expect(chunk).to.include('(https://github.com/insurgent-lab/conventional-changelog-insurgent/compare')
+        expect(chunk).to.include('](https://github.com/insurgent-lab/conventional-changelog-insurgent/commit/')
+        expect(chunk).to.include('](https://github.com/insurgent-lab/conventional-changelog-insurgent/issues/')
 
         i++
         cb()
